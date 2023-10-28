a2d82ee71b81513ccd7b6532f0c449d61f2e07f2
Karthik <msdhamodharan5@gmail.com>
2023-10-28 21:21:14 +0530

    shoe shopping app:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 function App() {
+  const allProducts = "https://fakestoreapi.com/products";
+  const [data, setData] = useState([]);
+
+  // Call API using Axios
+  useEffect(() => {
+    axios.get(allProducts).then((response) => setData(data));
+  }, [data]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title-app">Shopping App</div>
+
+      <div className="flexd">
+        {data?.map((item) => (
+          <div key={item.id} className="card">
+            <p> {item?.title}</p>
+            <p> {item?.description}</p>
+            <div>
+              <p>{item?.price}</p>
+            </div>
+            <img src={item?.image} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
